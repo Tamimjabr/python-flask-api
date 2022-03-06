dc4f8c8d19dfbaedd4534e36db3d2a6052c9216a
Tamim Jabr <tj222kg@student.lnu.se>
2022-03-06 13:12:15 +0100

    add post and delete and their tests

--- main.py
+++ main.py
@@ -1,41 +1,44 @@
 from flask import Flask, request
-from flask_restful import Api, Resource, reqparse
+from flask_restful import Api, Resource, reqparse, abort
 
 app = Flask(__name__)
 api = Api(app)
 
-names = {
-    "tamim": {
-        "age": 27,
-        "occupation": "student"
-    },
-    "Ida": {
-        "age": 25,
-        "occupation": "student"
-    }
-}
+video_post_args = reqparse.RequestParser()
+video_post_args.add_argument("name", type=str, required=True, help="Name cannot be blank")
+video_post_args.add_argument("views", type=str, required=True, help="views cannot be blank")
+video_post_args.add_argument("likes", type=str, required=True, help="likes cannot be blank")
 
-hello_post_args = reqparse.RequestParser()
-hello_post_args.add_argument("name", type=str, required=True, help="Name cannot be blank")
-hello_post_args.add_argument("views", type=str, required=True, help="views cannot be blank")
-hello_post_args.add_argument("likes", type=str, required=True, help="likes cannot be blank")
+videos = {}
 
 
-class HelloWorld(Resource):
+def abort_if_video_doesnt_exist(video_id):
+    if video_id not in videos:
+        abort(404, message="Video {} doesn't exist".format(video_id))
 
-    def get(self, name):
-        return names[name]
+def abort_if_video_exists(video_id):
+    if video_id in videos:
+        abort(409, message="Video {} already exists".format(video_id))
 
-    def post(self, name):
-        args = hello_post_args.parse_args()
-        return {
-            "name": args["name"],
-            "views": args["views"],
-            "likes": args["likes"]
-        }
+class Video(Resource):
+
+    def get(self, video_id):
+        abort_if_video_doesnt_exist(video_id)
+        return videos[video_id], 200
+
+    def post(self, video_id):
+        abort_if_video_exists(video_id)
+        args = video_post_args.parse_args()
+        videos[video_id] = args
+        return videos[video_id], 201
+
+    def delete(self, video_id):
+        abort_if_video_doesnt_exist(video_id)
+        del videos[video_id]
+        return '', 204
 
 
-api.add_resource(HelloWorld, '/helloworld/<string:name>')
+api.add_resource(Video, '/videos/<int:video_id>')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- test.py
+++ test.py
@@ -2,13 +2,18 @@ import requests
 
 BASE_URL = 'http://127.0.0.1:5000/'
 
-response = requests.get(BASE_URL + "helloworld/tamim")
-print(response.json())
+response = requests.get(BASE_URL + "videos/5")
+print(response.json(), response.status_code)
 
-
-response2= requests.post(BASE_URL + "helloworld/tamim", {
+response2 = requests.post(BASE_URL + "videos/5", {
     "likes": 10,
     "name": "tamim",
     "views": 100
 })
-print(response2.json())
+print(response2.json(), response2.status_code)
+
+response3 = requests.get(BASE_URL + "videos/5")
+print(response3.json(), response3.status_code)
+
+response4 = requests.delete(BASE_URL + "videos/5")
+print(response4, response4.status_code)
