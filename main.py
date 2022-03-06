ff843f7e10fd6079745e78aff97f8613f2714b88
Tamim Jabr <tj222kg@student.lnu.se>
2022-03-06 12:35:03 +0100

    use args to get data from body

--- main.py
+++ main.py
@@ -1,16 +1,41 @@
-from flask import Flask
-from flask_restful import Api, Resource
+from flask import Flask, request
+from flask_restful import Api, Resource, reqparse
 
 app = Flask(__name__)
 api = Api(app)
 
+names = {
+    "tamim": {
+        "age": 27,
+        "occupation": "student"
+    },
+    "Ida": {
+        "age": 25,
+        "occupation": "student"
+    }
+}
+
+hello_post_args = reqparse.RequestParser()
+hello_post_args.add_argument("name", type=str, required=True, help="Name cannot be blank")
+hello_post_args.add_argument("views", type=str, required=True, help="views cannot be blank")
+hello_post_args.add_argument("likes", type=str, required=True, help="likes cannot be blank")
+
 
 class HelloWorld(Resource):
-    def get(self):
-        return {'data':'helloworld'}
+
+    def get(self, name):
+        return names[name]
+
+    def post(self, name):
+        args = hello_post_args.parse_args()
+        return {
+            "name": args["name"],
+            "views": args["views"],
+            "likes": args["likes"]
+        }
 
 
-api.add_resource(HelloWorld, '/helloworld')
+api.add_resource(HelloWorld, '/helloworld/<string:name>')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- test.py
+++ test.py
@@ -2,5 +2,13 @@ import requests
 
 BASE_URL = 'http://127.0.0.1:5000/'
 
-response = requests.get(BASE_URL + "helloworld")
+response = requests.get(BASE_URL + "helloworld/tamim")
 print(response.json())
+
+
+response2= requests.post(BASE_URL + "helloworld/tamim", {
+    "likes": 10,
+    "name": "tamim",
+    "views": 100
+})
+print(response2.json())
